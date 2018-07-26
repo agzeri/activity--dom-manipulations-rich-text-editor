aa4ae60cbac37957ad15fcd36cbdca74e847f813
Yair <hello@agzeri.com>
2018-07-25 23:13:27 -0500

    nice and sweet exercise!

---
+++ js/app.js
@@ -0,0 +1,36 @@
+/**
+ * TASK 1: Add an event listener to the `<button class="rte-italics">`.
+ *
+ *         When user clicks, it should toggle the `fontStyle` property
+ *         value of the `<p class="textbox"></p>` to «italic».
+ */
+
+
+/**
+ * TASK 2: Add an event listener to the `<button class="rte-bold"></button>`.
+ *
+ *         When user clicks, it should toggle the `fontWeight` property
+ *         value of the `<p class="textbox"></p>` to 700.
+ */
+
+
+/**
+ * TASK 3:
+ * Add event listeners to the `<button class="rte-fontsize"></button>` elements.
+ *
+ *         When user clicks, it should change the `fontSize` property of the
+ *         `<p class="textbox"></p>` to the value of the button that was clicked.
+ */
+
+
+/**
+ * TASK 4:
+ * Add event listener to the `<button class="rte-fontcolor"></button>` elements.
+ *
+ *         When user clicks, it should take the value from the `data-color`
+ *         attribute and set the `color` property of the `<p class="textbox"></p>`
+ *
+ *         To access data-* attributes
+ *         https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+ */
+
